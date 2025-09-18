fafb864c02aeee5ffe99b68b8b9e5f73ff5cf8c7
estefanysuarez <laurasuarez24@gmail.com>
2025-09-18 10:07:45 -0400

    Fixed formatting

--- src/openneuro/tests/utils.py
+++ src/openneuro/tests/utils.py
@@ -1,12 +1,15 @@
-import json
+"""Utility functions for tests."""
+
 import os
+import json
 from pathlib import Path
 
-TEST_DATA_DIR = os.path.join(Path(__file__).parent, "data")
 
+TEST_DATA_DIR= os.path.join(Path(__file__).parent, "data")
 
 def load_json(path):
     """Load a JSON file."""
     path = os.path.join(TEST_DATA_DIR, path)
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
+
